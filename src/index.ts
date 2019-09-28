43342e8ddafe6bbcdbd10a8733ddc89e89d6b6e8
mtti <mtti@users.noreply.github.com>
2019-09-28 15:20:54 +0300

    Add removeFalsies()

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 export * from './functions/expectSingle';
 export * from './functions/fromEntries';
 export * from './functions/lastOf';
+export * from './functions/removeFalsies';
 export * from './functions/toArray';
+
+export * from './types/Falsy';
